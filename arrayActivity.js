39fe2f60b106c238174134407bc76767bc9652d4
merolley <merolly.caay11@gmail.com>
2024-10-16 19:25:05 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+//creating an array that store my 3 fav singers
+const favSinger = ["Fujii Kaze", "Ne-yo", "Adelle"];
+console.log(favSinger[0]); //printing the first singer using array index notation
+
+// storing my fav numbers in an array
+const favNumbers = [11, 4, 5, 3];
+const mixedArr = ["string", ["otherarray"], 123, true];
+for (let i=0; i <mixedArr.length; i++){
+    console.log(mixedArr[i]) //printing the items in the mixedArr
+}
